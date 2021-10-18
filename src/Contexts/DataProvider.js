149b12cbd60c181e5f300892190aec3d8bec0924
ZèD <imzihad@gmail.com>
2021-10-19 04:31:38 +0600

    Initial setup for firebase
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Contexts/DataProvider.js
+++ src/Contexts/DataProvider.js
@@ -1,12 +1,14 @@
 import React, { createContext, useState } from 'react';
 import useFetchServices from '../Hooks/useFetchServices';
+import useFirebase from '../Hooks/useFirebase';
 
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
     const [services, setServices] = useState([]);
     useFetchServices(setServices);
-    const allContext = { services };
+    const firebaseContext = useFirebase();
+    const allContext = { services, ...firebaseContext };
     return (
         <DataContext.Provider value={allContext}>
             {children}
