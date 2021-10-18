1e66dd34ce35c9a1f61d573e1f93c347bc3cfaf4
ZèD <imzihad@gmail.com>
2021-10-19 04:16:51 +0600

    Temporary Fix of not having a Dynamic API
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Services/ServiceDetails/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails/ServiceDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { Redirect, useParams } from 'react-router';
 import useProvider from '../../../Hooks/useProvider';
 
 const ServiceDetails = () => {
@@ -7,19 +7,24 @@ const ServiceDetails = () => {
     const serviceID = parseInt(serviceCode);
     const { services } = useProvider();
     const service = services.find(service => service.serviceID === serviceID)
-    const { serviceName, serviceImg, serviceDescription } = service;
-    return (
-        <div className="flex">
-            <div className="w-1/3">
-                <img className="w-full" src={serviceImg} alt={serviceName} />
+    if (service) {
+        const { serviceName, serviceImg, serviceDescription } = service;
+        return (
+            <div className="flex">
+                <div className="w-1/3">
+                    <img className="w-full" src={serviceImg} alt={serviceName} />
+                </div>
+                <div className="w-2/3 m-auto">
+                    <h1 className="text-8xl font-bold py-20">{serviceName}</h1>
+                    <p className="text-5xl font-semibold">{serviceDescription}</p>
+                    <button className="rounded-lg bg-green-600 text-white hover:bg-green-500 px-20 py-5 my-10">Make Appointment</button>
+                </div>
             </div>
-            <div className="w-2/3 m-auto">
-                <h1 className="text-8xl font-bold py-20">{serviceName}</h1>
-                <p className="text-5xl font-semibold">{serviceDescription}</p>
-                <button className="rounded-lg bg-green-600 text-white hover:bg-green-500 px-20 py-5 my-10">Make Appointment</button>
-            </div>
-        </div>
-    );
+        );
+    }
+    else{
+        return <Redirect to="/"></Redirect>
+    }
 };
 
 export default ServiceDetails;
