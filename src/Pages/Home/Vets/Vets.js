7d6359ecab79ad2fcfdfb02a59ccb5e95befc7ad
ZèD <imzihad@gmail.com>
2021-10-19 18:05:44 +0600

    Add Vet infos in Home
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Home/Vets/Vets.js
+++ src/Pages/Home/Vets/Vets.js
@@ -2,8 +2,85 @@ import React from 'react';
 
 const Vets = () => {
     return (
-        <div>
-            
+        <div className="bg-gray-200">
+            <h1 className="text-4xl font-semibold text-blue-400 py-5">Our Specialists</h1>
+            <div className="md:flex">
+                <div className="w-full flex flex-row justify-center items-center">
+                    <div className="card w-96 mx-auto bg-white  shadow-xl hover:shadow">
+                        <img className="w-32 mx-auto rounded -mt-20 border-8 border-white" src="https://i.ibb.co/bFD5rnK/doctor-carrying-little-gray-puppy.png" alt="" />
+                        <div className="text-center mt-2 text-3xl font-medium">Ajo Alex</div>
+                        <div className="text-center mt-2 font-semibold text-lg">@devpenzil</div>
+                        <div className="text-center font-bold text-xl">Kerala</div>
+                        <div className="px-6 text-center mt-2 font-semibold text-lg">
+                            <p>
+                                Pet health care specialist
+                            </p>
+                        </div>
+                        <hr className="mt-8" />
+                        <div className="flex p-4">
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">1.8 k</span> Followers
+                            </div>
+                            <div className="w-0 border border-gray-300">
+
+                            </div>
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">2.0 k</span> Following
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center">
+                    <div className="card w-96 mx-auto bg-white  shadow-xl hover:shadow">
+                        <img className="w-32 mx-auto rounded -mt-20 border-8 border-white" src="https://i.ibb.co/1zG1bDK/front-view-female-veterinarian-observing-little-dog-yellow-wall.png" alt="" />
+                        <div className="text-center mt-2 text-3xl font-medium">Ajo Alex</div>
+                        <div className="text-center mt-2 font-semibold text-lg">@devpenzil</div>
+                        <div className="text-center font-bold text-xl">Kerala</div>
+                        <div className="px-6 text-center mt-2 font-semibold text-lg">
+                            <p>
+                                Pet health care specialist
+                            </p>
+                        </div>
+                        <hr className="mt-8" />
+                        <div className="flex p-4">
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">1.8 k</span> Followers
+                            </div>
+                            <div className="w-0 border border-gray-300">
+
+                            </div>
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">2.0 k</span> Following
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center">
+                    <div className="card w-96 mx-auto bg-white shadow-xl hover:shadow">
+                        <img className="w-32 mx-auto rounded -mt-20 border-8 border-white" src="https://i.ibb.co/sFsrTJx/veterinarian-check-ing-puppy-s-health.png" alt="" />
+                        <div className="text-center mt-2 text-3xl font-medium">Ajo Alex</div>
+                        <div className="text-center mt-2 font-semibold text-lg">@devpenzil</div>
+                        <div className="text-center font-bold text-xl">Kerala</div>
+                        <div className="px-6 text-center mt-2 font-semibold text-lg">
+                            <p>
+                                Pet health care specialist
+                            </p>
+                        </div>
+                        <hr className="mt-8" />
+                        <div className="flex p-4">
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">1.8 k</span> Followers
+                            </div>
+                            <div className="w-0 border border-gray-300">
+
+                            </div>
+                            <div className="w-1/2 text-center">
+                                <span className="font-bold">2.0 k</span> Following
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
