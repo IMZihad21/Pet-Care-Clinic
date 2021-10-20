c15270cba341200ec9a23b324a460791f62c4e2e
ZèD <imzihad@gmail.com>
2021-10-20 23:33:48 +0600

    Slight changes in banner responsiveness
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,32 +4,31 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
     return (
-        <div className="md:px-10 md:mx-auto md:my-3">
+        <div className="px-2 md:px-10 md:mx-auto md:my-3">
             <Carousel
                 showArrows={true}
                 autoPlay={true}
-                centerMode={true}
                 infiniteLoop={true}
                 emulateTouch={true}
                 showThumbs={false}
             >
-                <div>
-                    <img src="https://i.ibb.co/dgwTkWD/carousel-1.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/dgwTkWD/carousel-1.png" alt="CarouselImages" />
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/nkkTTmw/carousel-2.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/nkkTTmw/carousel-2.png" alt="CarouselImages" />
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/DG2T0MS/carousel-3.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/DG2T0MS/carousel-3.png" alt="CarouselImages" />
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/vPM9N0M/carousel-4.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/vPM9N0M/carousel-4.png" alt="CarouselImages" />
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/w6dnGrD/carousel-5.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/w6dnGrD/carousel-5.png" alt="CarouselImages" />
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/z83f0pS/carousel-6.png" alt="CarouselImages" />
+                <div className="h-96 md:h-auto">
+                    <img className="h-full md:w-full rounded-xl" src="https://i.ibb.co/z83f0pS/carousel-6.png" alt="CarouselImages" />
                 </div>
             </Carousel>
         </div>
