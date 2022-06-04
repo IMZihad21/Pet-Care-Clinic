4f8421b8ccef879b8e99b4e5466d68331d06e62a
ZèD <imzihad@gmail.com>
2022-06-04 05:38:59 +0000

    Switched to gist hosted service json data
    Signed-off-by: GitHub <noreply@github.com>

--- src/Hooks/useServiceProvider.js
+++ src/Hooks/useServiceProvider.js
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 
 const useServiceProvider = () => {
     const [ services, setServices ] = useState([]);
+
+    const getServices = async () => {
+        const response = await fetch("https://gist.githubusercontent.com/IMZihad21/b77f5e38eb1a141d2471855a7c7d835f/raw/petCareServices.json");
+        const data = await response.json();
+        setServices(data);
+    }
+
     useEffect(() => {
-            fetch('/services.json')
-                .then(res => res.json())
-                .then(data => setServices(data))
+        getServices();
     }, [])
+
     return { services };
 };
 
